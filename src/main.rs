d3b6dcab5affb494fc377ebab0c54bd41254cb4a
mingxoxo <yesuel1111@naver.com>
2026-01-18 16:33:57 +0900

    Feat: Week 1 - Implement single command execution (ls -l)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-- Rust로 ls -l 실행 --");
+
+    let mut child = Command::new("ls")  // 실행 프로그램 이름
+            .arg("-l")                  // 인자
+            .spawn()                    // fork + exec
+            .expect("명령 실패");       // exec 실패 시
+
+    let ecode = child.wait()
+            .expect("자식 프로세스 기다리는 중 에러 발생");
+
+    println!("-- 실행 종료 (Exit code: {}) --", ecode);
 }
