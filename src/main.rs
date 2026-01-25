6bb8a5a36bb9884c58889001c6b250428a949d68
mingxoxo <yesuel1111@naver.com>
2026-01-25 17:29:32 +0900

    Feat: Week 2 - Implement basic pipe connection (ls | wc)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
-use std::process::Command;
-use std::env;
+use std::process::{Command, Stdio};
+// use std::env;
 
 fn main() {
-    // 1. 인자 수집
-    let args: Vec<String> = env::args().collect();
-  
-    if args.len() < 2 {
-        eprintln!("usage: cargo run -- <command> [args...]");
-        std::process::exit(1);
-    }
+    println!("--- Pipe 연결 시작: ls -l | wc -l ---");
 
-    // 2. 명령어 구성
-    // &를 사용하여 인자를 빌려옴(복사 X)
-    let mut cmd = Command::new(&args[1]);
+    // 1. 첫번째 명령
+    // 핵심: stdout을 'Stdio::piped()'로 설정
+    // 이것이 C의 pipe() 생성 + dup2(fd[1], STDOUT_FILENO) 역할을 동시에 진행
+    let mut child1 = Command::new("ls")
+            .arg("-l")
+            .stdout(Stdio::piped()) 
+            .spawn()
+            .expect("ls 실행 실패");
 
-    // 슬라이스로 한번에 넣기
-    cmd.args(&args[2..]);
+    // 2. pipe 연결 (C의 핵심 로직과 다른 부분)
+    // child1이 가진 stdout의 '소유권'을 가져옴 (take)
+    let child1_stdout = child1.stdout.take()
+                            .expect("stdout을 가져올 수 없음");
 
-    // 3. 실행 및 대기
-    let mut child = cmd.spawn().expect("명령 실행 실패");
-    let res = child.wait().expect("자식 프로세스 대기 실패");
+    // 3. 두번째 명령
+    // 핵심: stdin에 child1의 stdout을 꽂아줌
+    // 이것이 C의 dup2(fd[0], STDIN_FILENO) 역할
+    let mut child2 = Command::new("wc")
+            .arg("-l")
+            .stdin(Stdio::from(child1_stdout))
+            .spawn()
+            .expect("wc 실행 실패");
 
-    println!("--- exec result ---");
-    println!("Exit code: {}", res);
+    // 4. 종료 대기
+    child1.wait().expect("child1 wait 에러"); 
+    let output = child2.wait().expect("child2 wait 에러");
+
+    println!("--- 실행 종료 (Exit Code: {}) ---", output);
+
 }
